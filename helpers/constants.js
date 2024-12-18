d377d58c6061ba7ed9cf12da281f7ffb39d253fd
Travis Lawrence <travis.lawrence12@gmail.com>
2024-12-17 22:42:03 -0800

    fetching works

---
+++ helpers/constants.js
@@ -0,0 +1,3 @@
+const APOD_API_URL = "https://api.nasa.gov/planetary/apod";
+
+module.exports = { APOD_API_URL };
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,21 @@
 var express = require("express");
+const { APOD_API_URL } = require("../helpers/constants");
 var router = express.Router();
 
+const API_KEY = process.env.APOD_API_KEY;
+
 /* GET home page. */
-router.get("/", function (req, res, next) {
-  res.render("index", { title: "Express" });
+router.get("/apod", async function (req, res, next) {
+  const { date } = req.query;
+  const url = new URL(APOD_API_URL);
+  url.searchParams.append("api_key", API_KEY);
+  url.searchParams.append("date", date);
+
+  const apodReq = await fetch(url.toString());
+  const data = await apodReq.json();
+
+  console.log(data, "data");
+  res.render("index", { title: "Express", description: data.explanation });
 });
 
 module.exports = router;
